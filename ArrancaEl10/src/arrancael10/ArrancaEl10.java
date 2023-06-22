fbaf4cc5d47f0ed2c3b808c07d14f6526a8c2013
Max <max7214@gmail.com>
2023-06-21 21:15:28 -0300

    Update ArrancaEl10.java

--- ArrancaEl10/src/arrancael10/ArrancaEl10.java
+++ ArrancaEl10/src/arrancael10/ArrancaEl10.java
@@ -6,6 +6,10 @@
 package arrancael10;
 
 import clases.Matematica;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 /**
  *
@@ -16,7 +20,7 @@ public class ArrancaEl10 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {/*
         Matematica nuevo = new Matematica ();
         
         nuevo.setNumeroReal1(Math.random()*100);
@@ -29,7 +33,27 @@ public class ArrancaEl10 {
         System.out.println("calcularPotencia da: " + nuevo.calcularPotencia());
         System.out.println("calcularRaiz da: " + nuevo.calcularRaiz());
         
-        System.out.println("Prueba de devolver mayor static " + Matematica.devolverMayor(1,2));
+        System.out.println("Prueba de devolver mayor static " + Matematica.devolverMayor(1,2));*/
+        
+        /* EJERCICIO 12 creo que no necesitamos ayuda de nadie.
+        //No pregunten como lo hicimos
+        
+        String fecha1 = "2023-01-01";
+        String fecha2 = "2023-06-21";
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+
+        LocalDate fechaInicio = LocalDate.parse(fecha1, formatter);
+        LocalDate fechaFinal = LocalDate.parse(fecha2, formatter);
+
+        Period periodo = Period.between(fechaInicio, fechaFinal);
+
+        System.out.println("La diferencia entre las dos fechas es: "
+                + periodo.getYears() + " años, "
+                + periodo.getMonths() + " meses, y "
+                + periodo.getDays() + " días.");
+
+        */
     }
     
 }
