66e5c3ca803f89a2a64d926a7411a21639bbfe75
Max <max7214@gmail.com>
2023-06-28 19:29:27 -0300

    HolaMamaTengoNetflix

---
+++ EjercicioNetflix/src/Contenido/Alquiler.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Contenido;
+
+import java.util.Date;
+
+/**
+ *
+ * @author max72
+ */
+public class Alquiler {
+    Pelicula peliculaAlquilada;
+    Date fechaInicio;
+    Date fechaFin;
+    int precio;
+
+    public Alquiler(Pelicula peliculaAlquilada, Date fechaInicio, Date fechaFin, int precio) {
+        this.peliculaAlquilada = peliculaAlquilada;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.precio = precio;
+    }
+
+    public Alquiler() {
+    }
+
+    public Pelicula getPeliculaAlquilada() {
+        return peliculaAlquilada;
+    }
+
+    public void setPeliculaAlquilada(Pelicula peliculaAlquilada) {
+        this.peliculaAlquilada = peliculaAlquilada;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+}
---
+++ EjercicioNetflix/src/Contenido/Pelicula.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Contenido;
+
+/**
+ *
+ * @author max72
+ */
+public class Pelicula {
+    String titulo;
+    String genero;
+    int anio;
+    double duracion;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String titulo, String genero, int anio, double duracion) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anio = anio;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+    
+    
+}
